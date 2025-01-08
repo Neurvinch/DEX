a90cce761899cd0ac81d279ecb641722c16af592
Naveen pandian <npandian515@gmail.com>
2025-01-08 23:31:43 +0530

    i0o

--- dex_it/FrontEnd/src/App.js
+++ dex_it/FrontEnd/src/App.js
@@ -6,20 +6,30 @@ import './App.css';
 import Swap from './Components/Swap';
 
 import Tokens from  "./Components/Tokens"
+import { useConnect, useAccount } from 'wagmi';
+import {MetaMaskConnector} from 'wagmi/connectors/metaMask';
 
 function App() {
+       const {address , isConnected} = useAccount();
+       const {connect} = useConnect({
+        connector: new MetaMaskConnector(),
+       });
+           
+
+
+
+
   return (
-    <div >
-      <Header/>
-      <div>
-        <BrowserRouter>
-     <Routes>
-      <Route path ="/"  element ={<Swap/>}/>
-      <Route  path ="/tokens" element = {<Tokens/>} />
-     </Routes>
-     </BrowserRouter>
-      </div>
+    <div className="App">
+    <Header connect={connect} isConnected={isConnected} address={address} />
+    <div className="mainWindow">
+      <Routes>
+        <Route path="/" element={<Swap isConnected={isConnected} address={address} />} />
+        <Route path="/tokens" element={<Tokens />} />
+      </Routes>
     </div>
+
+  </div>
   );
 }
 
--- dex_it/FrontEnd/src/Components/Header.js
+++ dex_it/FrontEnd/src/Components/Header.js
@@ -1,27 +1,34 @@
-import React from 'react'
+import React from "react";
 import Logo from "../moralis-logo.svg";
-import eth from "../eth.svg";
-import  { BrowserRouter ,Link} from 'react-router-dom';
+import Eth from "../eth.svg";
+import { Link } from "react-router-dom";
 
+function Header(props) {
 
-const Header = () => {
+  const {address, isConnected, connect} = props;
+
   return (
-    <header className='header'>
-    <div className='leftSide'>
-        <img src= {Logo} alt='Logo' className='logo'/>
-        <BrowserRouter><Link to ="/" > <div className='headerItem'>Swap</div></Link> </BrowserRouter>
-       
-       <BrowserRouter> <Link to= "/tokens"><div className='headerItem'>Tokens</div></Link></BrowserRouter>
-        
-    </div>
-
-    <div  className='rightSide'>
-        < img src={eth} alt ="chain Logo"  className='eth'/>
-        <div className='connectButton'>Connect</div>
-    </div>
-    
+    <header>
+      <div className="leftH">
+        <img src={Logo} alt="logo" className="logo" />
+        <Link to="/" className="link">
+          <div className="headerItem">Swap</div>
+        </Link>
+        <Link to="/tokens" className="link">
+          <div className="headerItem">Tokens</div>
+        </Link>
+      </div>
+      <div className="rightH">
+        <div className="headerItem">
+          <img src={Eth} alt="eth" className="eth" />
+          Ethereum
+        </div>
+        <div className="connectButton" onClick={connect}>
+          {isConnected ? (address.slice(0,4) +"..." +address.slice(38)) : "Connect"}
+        </div>
+      </div>
     </header>
-  )
+  );
 }
- 
-export default Header
+
+export default Header;
